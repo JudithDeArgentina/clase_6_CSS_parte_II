f9a84855ca2f180ed61b5aa59a3e6129b5931544
Alejandra CAC <alejandra.j.gomez@gmail.com>
2022-05-24 20:10:19 -0300

    se agrego todo lo que tenia

---
+++ clase2405/index.js
@@ -0,0 +1,19 @@
+
+const username = document.getElementById('user');
+const password = document.getElementById('pasword');
+const btn = document.getElementById('userAccess');
+
+const validarCampos = (user,pass) => {
+    user.setCustomValidity('');
+    pass.setCustomValidity('');
+
+    if(username.value== ''){
+        username.setCustomValidity('El campo de user no puede estar vacío');
+    }else if (pass.value == ''){
+        pass.setCustomValidity('El campo de user no puede estar vacío');
+    }
+        username.setCustomValidity('El campo de user no puede estar vacío')
+    }
+
+
+// btn.addEventListener('click',(e)) => 
